a4ee70326ce37eb716829ac46391781a4ad61c8d
Steven Zheng <szheng.persevere@gmail.com>
2024-04-15 09:17:28 -0500

    used regex to validate email and then made conditionals for whether the email was valid or not

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const webObject = { original_url : 'https://freeCodeCamp.org', short_url : 1}
+const urls = []
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -20,9 +20,28 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post("/api/shorturl",(req,res)=>{
-  const {original_url,short_url} = webObject
-  res.json(original_url,short_url)
+app.post("/api/shorturl/:url?",(req,res)=>{
+  const validEmail = true
+  const {original_url,short_url} = req.body
+	const urlPattern = new RegExp('^(https?:\\/\\/)?'+ // validate protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // validate domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // validate OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // validate port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // validate query string
+    '(\\#[-a-z\\d_]*)?$','i'); // validate fragment locator
+  if(!req.params.url){
+    validEmail = !!urlPattern.test(original_url)
+    if(!validEmail){
+      res.json({error:"invalid url"})
+    } else {
+      urls.push({original_url,short_url})
+      res.json({original_url,short_url})
+    }
+  } else {
+    const filteredArr = urls.filter(url=> url.short_url === req.params.url)
+    const originalUrl = filteredArr.original_url
+    res.redirect(originalUrl)
+  }
 })
 
 app.listen(port, function() {
